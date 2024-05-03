04f1bce8a354c34cbfb0f511e0804c7260b98a16
bellasitech <158211804+bellasitech@users.noreply.github.com>
2024-05-03 13:56:40 -0400

    e

--- enemies.py
+++ enemies.py
@@ -1,1 +1,16 @@
-print("sildfhgblk.")
+x = input("you wake up in the middle of the night, thirsty. do you want to get a glass of water?")
+
+
+if x == 'yes':
+    direction = input('you get out of bed and open your door. forward left or right?')
+    if direction == 'left':
+        print('you move left. you bump into a wall. ouch! try going the other way')
+        direction = input('forward left or right?')
+    if direction == 'right': 
+        print('you move right. you bump into a wall. ouch! try going the other way')
+        direction = input('forward left or right?')
+    if direction == 'forward':
+        print('you move forward')
+        direction2 = input('which way do you want to go, left right or forward?')
+        if direction2 == 'left':
+            print('you encounter a monster!')
