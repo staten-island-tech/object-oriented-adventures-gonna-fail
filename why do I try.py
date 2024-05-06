7f4a5804c74af0c695be155ace4070ec6d4f47e9
KatelynSITH <158211680+KatelynSITH@users.noreply.github.com>
2024-05-06 14:00:29 -0400

    698l

--- why do I try.py
+++ why do I try.py
@@ -1,4 +1,13 @@
+print('ON NO THERE IS A MONSTER')
 
 import random
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(random.choice(numbers))
+
+class drag():
+    def __init__(info, damage, power, ):
+H = 20
+A = random.choice
+
+sum = (H-A)
+print(sum)
