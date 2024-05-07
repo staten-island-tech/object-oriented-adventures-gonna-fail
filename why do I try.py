9231a0249b90f1a977cf5c62219e96a234f31a4b
KatelynSITH <158211680+KatelynSITH@users.noreply.github.com>
2024-05-07 14:00:50 -0400

    tyk

--- why do I try.py
+++ why do I try.py
@@ -4,10 +4,25 @@ import random
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(random.choice(numbers))
 
-class drag():
-    def __init__(info, damage, power, ):
-H = 20
-A = random.choice
+class person():
+    def __init__(info):
+        info.damage = damage
+        info.health = health
 
-sum = (H-A)
-print(sum)
+    def attack(self):
+        x = self.health - self.damage
+        return x
+
+def encounter_one(char):
+    print(f"You have encountered {char.name}")
+class monster():
+    def __init__(inf):
+        inf.damag = damag
+        inf.healt = healt
+
+    def __str__(self):
+        return f"{self.name}, {self.age}, {self.artist}"
+     
+
+healt = 10
+damag = random.choice
