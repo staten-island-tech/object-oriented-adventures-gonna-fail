0ead8ac7d81fc48df48cd9dc78827b8ab2fc9e37
KatelynSITH <158211680+KatelynSITH@users.noreply.github.com>
2024-05-10 13:58:08 -0400

    ryu

--- why do I try.py
+++ why do I try.py
@@ -4,30 +4,25 @@ import random
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-class person():
-    self.value1 = 1
-    info.value2 = 2
-    def __init__(info, damage, health):
-        info.damage = damage
-        info.health = health
-
-    def attack(self):
-        x = 20 - (random.choice(numbers)) 
-        return x
-
-replaced_text = x
-print(replaced_text)
-
 def encounter_one(char):
     print(f"You have encountered {char.name}")
+
+class person():
+    def __init__(self, damage, health):
+        self.damage = damage
+        self.health = health
+
 class monster():
-    def __init__(inf):
-        inf.damag = damag
-        inf.healt = healt
+
+    def attack(self, monster):
+        monster.healt = monster.health - 5
+        print (monster.health)
+
+
+
 
     def __str__(self):
         return f"{self.name}, {self.age}, {self.artist}"
-     
 
-health = 10
-damag = random.choice
+
+Whalen = monster.healt = 10
