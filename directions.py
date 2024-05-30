ce0f68846589e3447056ed3df93abe5b5e0a1bbd
bellasitech <158211804+bellasitech@users.noreply.github.com>
2024-05-30 13:58:18 -0400

    Update directions.py

--- directions.py
+++ directions.py
@@ -1,7 +1,7 @@
 import random 
 y =['you bump into a wall. ouch! try going the other way', 'OH NO A MONSTER',':>']             
 
-class direction():
+class directions():
     def direction(self):
         direction = input('which way do you want to go, forward, left or right?')
         if direction == 'right':
@@ -12,11 +12,12 @@ class direction():
             self.forward
     def right():
         print('you move right')
-        print(random.choice)
+        print(random.choice(y))
     def forward():
         print('you move forward')
-        print(random.choice)
+        print(random.choice(y))
     def left():
         print('you move left')
-        print(random.choice)
-forward()
+        print(random.choice(y))
+
+directions.forward()
