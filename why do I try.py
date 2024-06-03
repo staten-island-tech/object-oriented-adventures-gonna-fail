fe93013cc2f3f99a721273808de92ca064ee4929
KatelynSITH <158211680+KatelynSITH@users.noreply.github.com>
2024-06-03 13:57:41 -0400

    uyk

--- why do I try.py
+++ why do I try.py
@@ -6,20 +6,39 @@ x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 class person():
 
-    def battle(self, monster):
-        monster.health = monster.health - self.damage
-        print(monster.health)
-        monster.health = health
+    def __init__(self, health, name, age, gender):
+        self.health = health
         self.damage = random.choice(x)
-        health = 5
-        damage = (random.choice(x))
-        hello = (health - damage)
-        print("Your health is: "+str(hello))
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Gender: {self.gender}")
+
+    def create():
+        name = input("enter your characters name")
+        age = int(input("enter characters age"))
+        gender = (input("enter characters gender"))
+        return person(name, age, gender)
+
+user = person.create()
+print(user.display())
+    
+def battle(self, monster):
+    monster.health = monster.health - self.damage
+    print(monster.health)
+    health = 5
+    monster = 6
+def add(y):
+    y = health - monster
+    print(y)
+    add(monster)
+
     
 
 
-    def __str__(self):
-        
-        print(f'my attack power is {self.health - self.damage}')
 
 
