24472c18565a465d3066a86290159bec3642d876
bellasitech <158211804+bellasitech@users.noreply.github.com>
2024-06-07 13:38:16 -0400

    Update directions.py

--- directions.py
+++ directions.py
@@ -1,3 +1,42 @@
+
+
+import random
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+class person():
+
+    def __init__(self, name, age, gender):
+        self.damage = random.choice(x)
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Gender: {self.gender}")
+
+    def create():
+        name = input("enter your characters name")
+        age = int(input("enter characters age"))
+        gender = (input("enter characters gender"))
+        return person(name, age, gender)
+        
+
+user = person.create()
+print(user.display())
+
+def battle(self, monster):
+    self.health = health
+    monster.health = monster.health - self.damage
+    print(monster.health)
+    health = 20
+    monster = random.choice()
+    y = health - monster
+    print(y)
+    battle(monster)
+
 import random 
 y =['you bump into a wall. ouch! try going the other way', 'OH NO A MONSTER',':>']             
 
@@ -5,7 +44,7 @@ class directions():
     def direction(self, y):
         direction = input('which way do you want to go, forward, left or right?')
         print(f'you moved {direction}')
-        return(random.choice(y))
+        printh(random.choice(y))
 
 
 x = directions()
