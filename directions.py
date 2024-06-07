145a666156de3bc30082bd1e3f17edb76cd52e74
bellasitech <158211804+bellasitech@users.noreply.github.com>
2024-06-07 13:53:16 -0400

    Update directions.py

--- directions.py
+++ directions.py
@@ -38,47 +38,47 @@ def battle(self, monster):
     battle(monster)
 
 import random 
-y =['you bump into a wall. ouch! try going the other way', 'OH NO A MONSTER',':>']             
+e =['you bump into a wall. ouch! try going the other way', 'OH NO A MONSTER',':>']             
 
 class directions():
-    def direction(self, y):
+    def direction(self, e):
         direction = input('which way do you want to go, forward, left or right?')
         print(f'you moved {direction}')
-        printh(random.choice(y))
+        print(random.choice(e))
 
 
 x = directions()
 a = input("you wake up in the middle of the night, thirsty. do you want to get a glass of water?")
 if a == 'yes':
     print('you get out of bed and open your door.')
-    monster = x.direction(y)
+    monster = x.direction(e)
     #if monster then call monster fight stuff
-    if random.choice(y) == 'OH NO A MONSTER':
-        x.people(y)
-    x.direction(y)
-    x.direction(y)
+    if random.choice(e) == 'OH NO A MONSTER':
+        person.battle(monster)
+    x.direction(e)
+    x.direction(e)
     print('you move forward and approach the staircase. carefully you walk down stairs, so you dont trip and fall')
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
     print('you move left and move into your kitchen! you succesfully acquire a glass of water and turn to go back to bed.')
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
     print('you move forward and approach the staircase. carefully you walk up stairs, so you dont trip and fall')
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
     print('you take a sip of water, set it aside and then go back to sleep')
 if a == 'no':
     print('goodbye')
