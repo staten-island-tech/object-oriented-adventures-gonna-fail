9e6d1815ef56ee96f074b0dc45d1d57dc8ddfac9
KatelynSITH <158211680+KatelynSITH@users.noreply.github.com>
2024-06-07 13:59:12 -0400

    hk

--- directions.py
+++ directions.py
@@ -44,11 +44,7 @@ class directions():
     def direction(self, e):
         direction = input('which way do you want to go, forward, left or right?')
         print(f'you moved {direction}')
-<<<<<<< HEAD
         print(random.choice(e))
-=======
-        print(random.choice(y))
->>>>>>> f4fbc4e9a2bdf9408e08a3787fc9d84c3eeaa515
 
 
 x = directions()
