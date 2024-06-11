5becf66b0ee83bc29f169ed2b8aacb8d05035a05
KatelynSITH <katelyna28@nycstudents.net>
2024-06-11 06:41:17 -0400

    rt

--- directions.py
+++ directions.py
@@ -1,5 +1,3 @@
-
-
 import random
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -11,6 +9,7 @@ class person():
         self.name = name
         self.age = age
         self.gender = gender
+        self.health = 20
 
     def display(self):
         print(f"Name: {self.name}")
@@ -27,60 +26,54 @@ class person():
 user = person.create()
 print(user.display())
 
-def battle(self, monster):
-    self.health = health
-    monster.health = monster.health - self.damage
-    print(monster.health)
-    health = 20
-    monster = random.choice()
-    y = health - monster
-    print(y)
-    battle(monster)
+
+def difference(health, damage):
+        return int(health - damage)
+
+health = user.health
+damage = (random.choice(x))
 
 import random 
-y =['you bump into a wall. ouch! try going the other way', 'OH NO A MONSTER',':>']             
+e =['you bump into a wall. ouch! try going the other way', 'OH NO A MONSTER',':>']             
 
 class directions():
-    def direction(self, y):
+    def direction(self, e):
         direction = input('which way do you want to go, forward, left or right?')
         print(f'you moved {direction}')
-        print(random.choice(y))
-
+        print(random.choice(e))
 
 x = directions()
 a = input("you wake up in the middle of the night, thirsty. do you want to get a glass of water?")
 if a == 'yes':
     print('you get out of bed and open your door.')
-    monster = x.direction(y)
+    monster = x.direction(e)
     #if monster then call monster fight stuff
-    if random.choice(y) == 'OH NO A MONSTER':
-        x.people(y)
-    x.direction(y)
-    x.direction(y)
+    if random.choice(e) == 'OH NO A MONSTER':
+        print(f'New health is {difference(health, damage)}')
+    x.direction(e)
+    x.direction(e)
     print('you move forward and approach the staircase. carefully you walk down stairs, so you dont trip and fall')
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
     print('you move left and move into your kitchen! you succesfully acquire a glass of water and turn to go back to bed.')
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
     print('you move forward and approach the staircase. carefully you walk up stairs, so you dont trip and fall')
-    x.direction(y)
-    x.direction(y)
-    x.direction(y)
+    x.direction(e)
+    x.direction(e)
+    x.direction(e)
     print('you take a sip of water, set it aside and then go back to sleep')
 if a == 'no':
     print('goodbye')
-elif:
-    print('retry')
